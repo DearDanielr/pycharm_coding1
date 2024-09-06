55fcdc95b4441ff0cd7ab3f82fa1d70f96b1c805
Seabass319 <bryanseabass319@gmail.com>
2024-09-06 13:36:29 -0600

    1

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,1 +1,12 @@
-test
+#set variables
+a = 272
+b = 17
+
+#print a and b
+print(f"a = {a} \nb= {b}\n")
+
+#calculations
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a * b = {a * b}")
+print(f"a / b = {a // b}")
